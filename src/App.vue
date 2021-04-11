4292b5e0b4d75fcc273879bc229ae0d42329745b
arturgawlik <artur.gawlik@icloud.com>
2021-04-11 20:56:08 +0200

    clean up

--- src/App.vue
+++ src/App.vue
@@ -2,36 +2,15 @@
 https://my.api.mockaroo.com/customers.json?key=f3d1e2f0 -->
 
 <template>
-  <div id="nav">
-    <nav>
-      <router-link to="/"><img src="./assets/logo-small.png"/></router-link>
-      <router-link to="/">Car rental</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/Users">Users</router-link>
-    </nav>
-  </div>
   <router-view />
 </template>
 
+<script>
+
+export default {
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #018850;
-}
-
-#nav a.router-link-exact-active {
-  color: #05363f;
-}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,14 @@
 <template>
-
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
-  <div class="alert alert-success alert-dismissible fade show" role="alert">
-  With Bootstrap!
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-    <span aria-hidden="true">&times;</span>
-  </button>
-</div>
+  <div>hello world</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
-
   },
 };
 </script>
