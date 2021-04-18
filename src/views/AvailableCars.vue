1aa451b33da707887d2c60e1ac2c53d1ac5654d0
arturgawlik <artur.gawlik@icloud.com>
2021-04-18 17:21:19 +0200

    feat: add shadow on card hover

--- src/views/AvailableCars.vue
+++ src/views/AvailableCars.vue
@@ -3,8 +3,8 @@
     <p class="text-4xl text-center mx-auto mt-28">
       Wybierz samochód którym jesteś zainteresowany
     </p>
-    <div class="w-full flex m-10 flex-wrap justify-center w-">
-      <div class="flex flex-col m-5 bg-white rounded-lg p-10 pb-0 w-80 justify-self-center" v-for="car in carList" :key="car.id">
+    <div class="w-full flex m-10 flex-wrap justify-center">
+      <div class="hover:shadow-2xl flex flex-col m-5 bg-white rounded-lg p-10 pb-0 w-80 justify-self-center" v-for="car in carList" :key="car.id">
         <router-link class="cursor-pointer" :to="`/available-cars/${car.id}`">
           <img :src="car.image" alt="car image" class="h-32 rounded-lg">
         </router-link>
