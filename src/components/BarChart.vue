367defa3aa48387c985e5035b20256d3f9fbcbba
Adam <adaszet@poczta.fm>
2021-04-19 22:21:36 +0200

    Bar and pie chart

---
+++ src/components/BarChart.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <canvas :id="chartId" />
+  </div>
+</template>
+
+<script>
+import {Chart, registerables} from "chart.js";
+
+export default {
+  name: "BarChart",
+  props: {
+    chartId: {
+      type: String,
+      default: 'chartId',
+      required: true
+    },
+    dataset: {
+      type: Array,
+      default: () => []
+    },
+    options: {
+      type: Object,
+      default: () => ({
+        scales: {
+          xAxes: [{
+            type: 'time',
+            time: {
+              displayFormats: { day: 'DD' }
+            }
+          }]
+        },
+        tooltips: { enabled: true },
+        legend: { display: false },
+        elements: { point: { radius: 0 } }
+      })
+    }
+  },
+  created() {
+    Chart.register(...registerables)
+  },
+  mounted () {
+    new Chart(this.chartId, {
+      type: 'bar',
+      data: {
+        datasets: this.dataset
+      },
+      options: this.options
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/PieChart.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <canvas :id="chartId" />
+  </div>
+</template>
+
+<script>
+import {Chart, registerables} from "chart.js";
+export default {
+  name: "PieChart",
+  props: {
+    chartId: {
+      type: String,
+      default: 'chartId',
+      required: true
+    },
+    dataset: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  created() {
+    Chart.register(...registerables)
+  },
+  mounted () {
+    new Chart(this.chartId, {
+      type: 'pie',
+      data: this.dataset
+    })
+  }
+}
+</script>
+
+<style scoped>
+</style>
